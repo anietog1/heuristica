7b2e22ab1f280a8d7a05df5bbbf4b5ff8949959c
Agustín Nieto García <anietog1@eafit.edu.co>
2020-01-26 23:10:21 -0500

    Finalizando la solución n^3. Queda pendiente el punto fijo

--- smsp-tardanza-ponderada.cpp
+++ smsp-tardanza-ponderada.cpp
@@ -11,6 +11,7 @@ using namespace std;
 
 struct proc {
   int id, p, d, w; // id, duracion, tiempo limite, peso
+  int t, tp; // tardanza, tardanza ponderada
 };
 
 int n;
@@ -25,41 +26,46 @@ int calc_t() {
 
   for(int i = 0; i < n; ++i) {
     t += procs[i].p;
-
-    if(t > procs[i].d)
-      res += (t - procs[i].d) * procs[i].w;
+    procs[i].t = max(0, (t - procs[i].d));
+    procs[i].tp = procs[i].t * procs[i].w;
+    res += procs[i].tp;
   }
 
   return res;
 }
 
-void solve_fixed_point() {
-  int min_t = (1 << 30), curr_t = (1 << 30 - 1);
-  proc min_ans[n], curr_ans[n];
+// void solve_fixed_point() {
+//   int min_t = (1 << 30), curr_t = (1 << 30 - 1);
+//   proc min_ans[n], curr_ans[n];
 
-  while(curr_t < min_t) {
-    min_t = curr_t;
-    for(int i = 0; i < n; ++i) min_ans[i] = curr_ans[i];
+//   while(curr_t < min_t) {
+//     min_t = curr_t;
+//     for(int i = 0; i < n; ++i) min_ans[i] = curr_ans[i];
 
-    for(int i = 0; i < n; ++i) {
-      for(int j = 0; j < n; ++j) {
-        swap(curr_ans[i], curr_ans[j]);
-        int temp_t = calc_t();
+//     for(int i = 0; i < n; ++i) {
+//       for(int j = 0; j < n; ++j) {
+//         swap(curr_ans[i], curr_ans[j]);
+//         int temp_t = calc_t();
 
-        if(temp_t < curr_t) {
-          swap(curr_ans[i], curr_ans[j]);
-        } else {
-          swap(curr_ans[i], curr_ans[j]);
-        }
-      }
-    }
-  }
-}
+//         if(temp_t < curr_t) {
+//           swap(curr_ans[i], curr_ans[j]);
+//         } else {
+//           swap(curr_ans[i], curr_ans[j]);
+//         }
+//       }
+//     }
+//   }
+// }
 
 void solven3() {
   sort(procs, procs + n, comp_risk_first);
 
   int min_t = calc_t();
+  // [ i k j ]
+  // i asegura un lower bound que dejaremos de mover,
+  // j determina el elemento que estamos acomodando actualmente
+  // k sirve de indice para ir cambiando con j
+  // el concepto es similar al bubble sort, donde vamos acomodando elemento por elemento "optimamente"
   for(int i = 0; i < n; ++i) {
     for(int j = i; j < n; ++j) {
       for(int k = i; k < j; ++k) {
@@ -79,15 +85,18 @@ int main(int argc, char** argv, char** env) {
     cin >> procs[i].p >> procs[i].d >> procs[i].w;
   }
 
-  //solve_fixed_point();
   solven3();
 
-  cout << "id p d w" << endl;
+  int ans = calc_t();
+  cout << "id p d w t tp" << endl;
   for(int i = 0; i < n; ++i) {
     cout << procs[i].id << " "
          << procs[i].p << " "
          << procs[i].d << " "
-         << procs[i].w << endl;
+         << procs[i].w << " "
+         << procs[i].t << " "
+         << procs[i].tp << " "
+         << endl;
   }
-  cout << endl << "Tardanza ponderada total: " << calc_t() << endl;
+  cout << endl << "Tardanza ponderada total: " << ans << endl;
 }
