d84bce32b59269114a9d6175ea4f7cfb5075c7b4
Agustín Nieto García <anietog1@eafit.edu.co>
2020-02-11 23:41:53 -0500

    Solo sorting

--- fssws.cpp
+++ fssws.cpp
@@ -6,6 +6,7 @@ using namespace std;
 #define forn(i, n) for(int i = 0; i < n; ++i)
 #define fore(i, a, b) for(int i = a; i <= b; ++i)
 #define INF (1 << 30)
+#define pii pair<int, int>
 
 int n = 0, m = 0;
 int p[MAXN][MAXN] = {};
@@ -13,18 +14,36 @@ int p[MAXN][MAXN] = {};
 int order[MAXN] = {};
 int t[MAXN][MAXN] = {};
 
-int calcZ() {
-  int curr, prev;
-
-  fore(j, 1, n) {
-    curr = order[j];
-    prev = order[j - 1];
-
-    fore(k, 1, m)
-      t[curr][k] = max(t[prev][k] + p[prev][k], t[curr][k - 1] + p[curr][k - 1] + 1);
-  }
-
+int calcZ(int j) {
+  int curr = order[j], prev = order[j - 1];
+  fore(k, 1, m)
+    t[curr][k] = max(t[prev][k] + p[prev][k], t[curr][k - 1] + p[curr][k - 1]);
   return t[curr][m] + p[curr][m];
+}
+
+int calcZ() {
+  fore(j, 1, n - 1)
+    calcZ(j);
+  return calcZ(n);
+}
+
+int calcZ10(int j) {
+  int curr = order[j], prev = order[j - 1];
+  fore(k, 1, m) {
+    int _t, _p = p[curr][k];
+    _t = max(t[prev][k] + p[prev][k], t[curr][k - 1] + p[curr][k - 1]);
+    if((_t + _p) / 10 == _t / 10 || (_t + _p) % 10 == 0 && _p <= 10)
+      t[curr][k] = _t;
+    else
+      t[curr][k] = _t + (10 - _t % 10);
+  }
+  return t[curr][m] + p[curr][m];
+}
+
+int calcZ10() {
+  fore(j, 1, n - 1)
+    calcZ10(j);
+  return calcZ10(n);
 }
 
 int main() {
@@ -32,20 +51,24 @@ int main() {
   fore(j, 1, n) fore(k, 1, m) cin >> p[j][k];
   fore(j, 0, n) order[j] = j;
 
-  int bestZ = INF;
-  int bestOrder[n] = {};
+  int z = INF, z10 = INF;
 
-  do {
-    int tmpZ = calcZ();
+  // los trabajos con mayor suma(p[j][k] ** 2) van primero
+  // sorpresivamente da peor resultado que no sortear
+  pii pondered[n + 1];
+  fore(j, 1, n) {
+    pondered[j].second = j;
+    pondered[j].first = 0;
+    fore(k, 1, m) pondered[j].first += p[j][k] * p[j][k];
+  }
+  sort(pondered + 1, pondered + 1 + n);
+  fore(j, 1, n) order[j] = pondered[j].second;
 
-    if(tmpZ < bestZ) {
-      bestZ = tmpZ;
-      memcpy(bestOrder, order + 1, n * sizeof(int));
-    }
-  } while(next_permutation(order + 1, order + n + 1));
-
-  cout << "min Z = " << bestZ << endl;
+  z = calcZ();
+  cout << "z = " << z << endl;
+  z10 = calcZ10();
+  cout << "z10 = " << z10 << endl;
   cout << "order:";
-  forn(i, n) cout << " " << bestOrder[i];
+  fore(i, 1, n) cout << " " << order[i];
   cout << endl;
 }
