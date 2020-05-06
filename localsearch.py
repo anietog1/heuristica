f62585e8d12a3c20a3cef131f7ba5cbbc9a542d4
Agustín Nieto <anietog1@eafit.edu.co>
2020-05-06 17:43:33 -0500

    Con ayuda del grandísimo esta mierda funciona

--- localsearch.py
+++ localsearch.py
@@ -2,26 +2,56 @@ import sys
 import copy
 import util
 
+# mueve los trabajos a la derecha llenando huequitos
 def move_right(n, m, L, p, t, start, finish):
-    for k in reversed(range(m)):
-        prev_machine = None if k == 0 else t[k - 1]
-        machine = t[k]
-        next_machine = None if k == m - 1 else t[k + 1]
+    for machine in reversed(range(m)):
+        for job_idx in reversed(range(n)):
+            if machine == m - 1 and job_idx == n - 1:
+                # ultimo trabajo se deja quieto
+                continue
 
-        for j in reversed(range(n)):
-            # si hay espacio entre este y el siguiente, moverlo hasta el final
-            # sin que quede cruzando un turno
+            job, _ = t[machine][job_idx]
+            duration = p[job][machine]
 
-            # después, para cada dos trabajos que aparezcan siguientes, ver si
-            # hay un huequito en el que quepa
+            max_finish = util.INF
 
-            # nota: debo garantizar que los finales de turno siempre están ocupados
-            # entonces yo se que el trabajo anterior finalizó al final del turno actual o justo antes de mi
-            job = machine[j]
-            late_end = finish[j][k]
+            if machine < m - 1:
+                max_finish = start[job][machine + 1]
 
-            if j < n - 1:
-                late_end = None
+            if job_idx == n - 1:
+                # al ultimo trabajo de cada maquina
+                # se le aplica solamente con el max_finish
+                # solo debe garantizar que termina antes de que empieza en la sig
+                _finish = util.finish_if(max_finish, duration, L)
+                start[job][machine] = _finish - duration
+                finish[job][machine] = _finish
+                t[machine][job_idx] = (job, start)
+            else:
+                # cuadro con el siguiente en la posicion actual
+                next_job, _ = t[machine][job_idx + 1]
+                _finish = util.finish_if(start[next_job][machine], duration, L)
+                _start = _finish - duration
+                start[job][machine] = _start
+                finish[job][machine] = _finish
+                t[machine][job_idx] = _start
+
+                # busco una mejor posicion
+                for right_idx in reversed(range(job_idx + 2, n)):
+                    left_job, _ = t[machine][right_idx - 1]
+                    left_finish = finish[left_job][machine]
+
+                    right_job, _ = t[machine][right_idx]
+                    right_start = start[right_job][machine]
+
+                    possible_finish = util.finish_if(right_start, duration, L)
+                    possible_start = possible_finish - duration
+
+                    if possible_finish <= max_finish and possible_start >= left_finish:
+                        start[job][machine] = possible_start
+                        finish[job][machine] = possible_finish
+                        t[machine].insert(right_idx, (job, possible_start))
+                        t[machine].pop(job_idx)
+                        break
 
 def move_left(n, m, L, p, t, start, finish):
     # lo mismo de arriba pero garantizando que todo trabajo inicia en un turno o inmediatamente
