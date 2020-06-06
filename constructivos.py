5cbf8e2b917bcd41ac8be9c041696cea07ea8aa8
Agustin Nieto Garcia <anietog1@eafit.edu.co>
2020-06-06 14:12:42 -0500

    Corrigiendo el NEH.

--- constructivos.py
+++ constructivos.py
@@ -6,7 +6,7 @@ def neh(n, m, L, p):
     chosen = []
     available = [job for job in range(n)]
 
-    duration = [sum(p[job]) for job in range(n)]
+    duration = [calc_z(n, m, L, p, [job] * m) for job in range(n)]
     available.sort(key=lambda job: duration[job])
 
     while available:
@@ -17,42 +17,41 @@ def neh(n, m, L, p):
 
         for i in range(len(chosen) + 1):
             chosen.insert(i, j)
-
-            rcl = [chosen[i] for _ in range(m) for i in range(len(chosen))]
-
-            t = [[] * n for _ in range(m)]
-            start = [[None] * m for _ in range(n)]
-            finish = [[None] * m for _ in range(n)]
-
-            machines = [0] * n
-            tempz = 0
-
-            for job in rcl:
-                machine = machines[job]
-                machines[job] += 1
-
-                _start = 0
-
-                if len(t[machine]) > 0:
-                    prev_job = t[machine][-1]
-                    _start = max(_start, finish[prev_job][machine])
-
-                if machine > 0:
-                    _start = max(_start, finish[job][machine - 1])
-
-                _start = util.start_with_shift(_start, p[job][machine], L)
-
-                start[job][machine] = _start
-                finish[job][machine] = _start + p[job][machine]
-                tempz = max(tempz, finish[job][machine])
+            tempz = calc_z(n, m, L, p, chosen)
 
             if tempz < minz:
                 minz = tempz
                 mini = i
 
             chosen.pop(i)
-
         chosen.insert(mini, j)
 
-    rcl = [chosen[i] for _ in range(m) for i in range(len(chosen))]
+    rcl = [chosen[i] for _ in range(m) for i in range(n)]
     return rcl
+
+def calc_z(n, m, L, p, chosen):
+    t = [[] for _ in range(m)]
+    start = [[None] * m for _ in range(n)]
+    finish = [[None] * m for _ in range(n)]
+    z = 0
+
+    for job in chosen:
+        for machine in range(m):
+            _start = 0
+            duration = p[job][machine]
+
+            if len(t[machine]) > 0:
+                prev_job = t[machine][-1]
+                _start = max(_start, finish[prev_job][machine])
+
+            if machine > 0:
+                _start = max(_start, finish[job][machine - 1])
+
+            _start = util.start_with_shift(_start, duration, L)
+
+            t[machine].append(job)
+            start[job][machine] = _start
+            finish[job][machine] = _start + duration
+            z = max(z, finish[job][machine])
+
+    return z
